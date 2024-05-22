6551adc4ba42e105a0fe8375142c1dd2e4e49512
Mohamed Ahmed <ma07041705@gmail.com>
2024-05-22 17:51:18 +0300

    Object Inheritance Lab Part One

--- lib/teacher.py
+++ lib/teacher.py
@@ -5,6 +5,9 @@ from user import User
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Python Basics", "OOP Concepts", "Data Structures", "Algorithms"]
 
     def teach(self):
-        pass
+        return random.choice(self.knowledge)
